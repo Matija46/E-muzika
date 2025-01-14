a69178ea1eb41f25c48c138633ed1a1c60422eba
Matija46 <matija.basa123@gmail.com>
2025-01-14 14:21:34 +0100

    commit11

--- obj/Debug/net8.0/web.AssemblyInfo.cs
+++ obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6dbae8cecd1fe4ef6715ef9af4addf8276dae51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef9d437c154aa518bf7eab93a3e222f3c38e8f72")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
